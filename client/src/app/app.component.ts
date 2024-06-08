eb74ea8f6a71162ae3bcf619237f9af16e5493af
fabiobragato <fabio_bragato@hotmail.com>
2024-06-08 16:53:44 -0300

    Add: adicionado a lib PrimeNg para utilização no frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,12 +10,20 @@ export class AppComponent implements OnInit {
 
   title = "Angular 10 CRUD Application";
   usuarios: any;
+  cols: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<any>('https://localhost:5001/api/Usuarios').subscribe({
-      next: response => this.usuarios = response,
+      next: response => {
+        this.usuarios = response;
+        this.cols = [
+          { field: 'id', header: 'ID' },
+          { field: 'nome', header: 'Nome' },
+          { field: 'email', header: 'E-mail' }
+        ];
+      },
       error: error => console.error(error),
       complete: () => console.log('Request completed')
     });
